63ecc35ebed2e7ec8cde4eb2cc88e7c962e85aa3
Caroly Fisher <carolyfisher@gmail.com>
2016-05-22 01:27:23 -0400

    Sam

--- scripts.js
+++ scripts.js
@@ -1,7 +1,26 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 100) {
-        $('.mdl-layout__tab-bar').addClass('fixed');
-    } else {
-        $('.mdl-layout__tab-bar').removeClass('fixed');
+$(function(){
+    $('#thing').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('#thing').data('size') == 'big')
+        {
+            $('#thing').data('size','small');
+            $('#thing').stop().animate({
+                height:'40px'
+            },600);
+        }
+    }
+    else
+    {
+        if($('#thing').data('size') == 'small')
+        {
+            $('#thing').data('size','big');
+            $('#thing').stop().animate({
+                height:'100px'
+            },600);
+        }  
     }
 });
